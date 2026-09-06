70a14eca0bd8bb81ef7ffd1dbbbc00a3cb3d4df2
bitbyrubayat <mdrubayatalam@gmail.com>
2026-09-06 20:09:11 +0600

    Todo Part 1-5

--- todo_1.js
+++ todo_1.js
@@ -13,15 +13,67 @@ addBtn.addEventListener("click", ()=>{
     // console.log("btn-clicked");
     // console.log(`${taskInput.value}`);
     let li = document.createElement("li")
-    li.textContent = taskInput.value;
+    // li.textContent = taskInput.value;
+    // taskList.appendChild(li);
+    // taskInput.value = "";
+
+    let span = document.createElement("span");
+ span.textContent = taskInput.value;
+let btnWrapper = document.createElement("span");
+
+let completeBtn = document.createElement("button");
+completeBtn.textContent = "✅";
+let deleteBtn = document.createElement("button");
+deleteBtn.textContent = "❌";
+
+li.appendChild(span);
+li.appendChild(btnWrapper);
+
+
+
+btnWrapper.appendChild(completeBtn);
+btnWrapper.appendChild(deleteBtn);
+
+if(taskInput.value != ""){
     taskList.appendChild(li);
+}
+else return 0;
+
+
+taskInput.value = "";
+
+ 
 })
 
+
+
 taskInput.addEventListener("keydown",(event)=>{
-    if (event.key === "Enter"){
+    if (event.key === "Enter" && taskInput.value != ""){
     let li = document.createElement("li")
-    li.textContent = taskInput.value;
+    // li.textContent = taskInput.value;
+    // taskList.appendChild(li);
+    // taskInput.value = "";
+   
+    let span = document.createElement("span");
+    span.textContent = taskInput.value;
+    let btnWrapper = document.createElement("span");
+
+    let completeBtn = document.createElement("button");
+    completeBtn.textContent = "✅";    
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+
+    btnWrapper.appendChild(completeBtn);
+    btnWrapper.appendChild(deleteBtn);
+   
+    li.appendChild(span);
+    li.appendChild(btnWrapper);
+
     taskList.appendChild(li);
+
+    taskInput.value = "";
+    
+
     }
 })
 
